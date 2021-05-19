913871351889699aff68f812d79d048b114eae6c
Henrique <henrique.thsantos.ths@gmail.com>
2021-05-19 11:46:17 -0300

    Componente Post

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { View } from "react-native";
 import Header from "./src/components/Header";
+import Post from "./src/components/Post";
 
 export default function App() {
-    return <Header />;
+    return (
+        <View style={{ flex: 1 }}>
+            <Header />
+            <Post image={require("./assets/imgs/fence.jpg")} />
+        </View>
+    );
 }
