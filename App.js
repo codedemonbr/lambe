34c2a657af36cc1e8491f9aac9f03389c60da502
Henrique <henrique.thsantos.ths@gmail.com>
2021-05-19 17:28:30 -0300

    Adicionando Comentários

--- App.js
+++ App.js
@@ -4,10 +4,23 @@ import Header from "./src/components/Header";
 import Post from "./src/components/Post";
 
 export default function App() {
+    const comments = [
+        {
+            nickname: "Joana Elena Silva",
+            comment: "Excelente Foto!",
+        },
+        {
+            nickname: "Rafael Gustavo Pereira",
+            comment: "Muito ruim! Faça melhor...",
+        },
+    ];
     return (
         <View style={{ flex: 1 }}>
             <Header />
-            <Post image={require("./assets/imgs/fence.jpg")} />
+            <Post
+                image={require("./assets/imgs/fence.jpg")}
+                comments={comments}
+            />
         </View>
     );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Image, Dimensions } from "react-native";
 import Author from "./Author";
+import Comments from "./Comments";
+import AddComment from "./AddComment";
 
 class Post extends Component {
     render() {
@@ -8,6 +10,8 @@ class Post extends Component {
             <View style={styles.container}>
                 <Image source={this.props.image} style={styles.image} />
                 <Author email="fulano@gmail.com" nickname="Fulano de Tal" />
+                <Comments comments={this.props.comments} />
+                <AddComment />
             </View>
         );
     }
